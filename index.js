51e38b283f60567582bff0fe66131d68c5eb7818
Matiitii <166016459+Matiitii@users.noreply.github.com>
2024-04-04 18:39:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+function calcular() {
+    let N1 = parseFloat(document.querySelector('#Num1').value)
+
+    let N2 = parseFloat(document.querySelector('#Num2').value)
+
+    let r = N1 + N2
+    
+    Resultado1.innerHTML = r
+}
+
+function calcular2() {
+    let N1 = parseFloat(document.querySelector('#Num1').value)
+
+    let N2 = parseFloat(document.querySelector('#Num2').value)
+
+    let r = N1 - N2
+    
+    Resultado1.innerHTML = r
+}
+
+function calcular3() {
+    let N1 = parseFloat(document.querySelector('#Num1').value)
+
+    let N2 = parseFloat(document.querySelector('#Num2').value)
+
+    let r = N1 * N2
+    
+    Resultado1.innerHTML = r
+}
+
+function calcular4() {
+    let N1 = parseFloat(document.querySelector('#Num1').value)
+
+    let N2 = parseFloat(document.querySelector('#Num2').value)
+
+    let r = N1 / N2
+    
+    Resultado1.innerHTML = r
+}
+
+let texto = "Tu edad es "
+function edad() {
+    let N1 = parseFloat(document.querySelector('#NumAC').value)
+
+    let N2 = parseFloat(document.querySelector('#NumAN').value)
+
+    let r = N1 - N2
+    
+    Resultado2.innerHTML = '¡'+ texto + r+ '!'
+}
+
+function bienvenida() {
+    let NombreAlumno= (document.querySelector('#Nombre').value + ' ')
+    let ApellidoAlumno= (document.querySelector('#Apellido').value)
+     let NombreYApellido = NombreAlumno + ApellidoAlumno
+     let texto1 = 'Bienvenid@ '+ NombreYApellido
+     Resultado3.innerHTML = '¡' +texto1+ '!'
+}
+
+//let boton = document.querySelector ('#calcu')
+
+//boton.style.backgruound.color = 'red'
+
+
